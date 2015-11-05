6a0e32a99bf33ca14fedebc02a7f4d78b5472c22
Daniel Kristiyanto <danielkr@uw.edu>
2015-11-05 21:27:51 +0000

    init

---
+++ WP-CONTENT/themes/uw-2014-master/content.php
@@ -0,0 +1,29 @@
+<?php
+if (is_single() || is_home()){
+    the_date('F j, Y', '<p class="date">', '</p>');
+}
+?>
+<h1><?php the_title() ?></h1>
+<?php
+if ((is_single() || is_home()) && get_option('show_byline_on_posts')) :
+?>
+<div class="author-info">
+    <?php the_author(); ?>
+    <p class="author-desc"> <small><?php the_author_meta(); ?></small></p>
+</div>
+<?php
+endif;
+  if ( ! is_home() && ! is_search() && ! is_archive() ) :
+    uw_mobile_menu();
+  endif;
+
+?>
+
+<?php
+
+  if ( is_archive() || is_home() )
+    the_excerpt();
+  else
+    the_content();
+    //comments_template(true);
+ ?>
---
+++ WP-CONTENT/themes/uw-2014-master/setup/class.uw-shortcodes.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This is the object that holds all the UW shortcodes
+ * Shortcodes are how we get functionality in content for power users
+ */
+
+class UW_Shortcodes
+{
+    function __construct()
+    {
+        $this->includes();
+        $this->initialize();
+    }
+
+    private function includes()
+    {
+        require_once('class.tile-box-shortcode.php');
+        require_once('class.button-shortcode.php');
+        require_once('class.youtube-shortcode.php');
+        require_once('class.trumba-shortcode.php');
+        require_once('class.subpage-list-shortcode.php');
+        require_once('class.accordion-shortcode.php');
+    }
+
+    private function initialize()
+    {
+        $this->tile_box       = new TileBox();
+        $this->button         = new UW_Button();
+        $this->youtube        = new UW_YouTube();
+        $this->trumba         = new UW_Trumba();
+        $this->subpage_list   = new UW_SubpageList();
+        $this->accordion      = new UW_AccordionShortcode();
+    }
+}
